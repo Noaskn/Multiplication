114c211c8b3b617a30ae8bda0924a205ddd65076
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-03-28 13:41:19 +0100

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -1,13 +1,40 @@
-let num1 = document.getElementById("num1");
-let num2 = document.getElementById("num2");
-
-function setNum() {
-    let n = Math.floor(Math.random()*10) + 1;
-    let m = Math.floor(Math.random()*10) + 1;
-    num1.innerText = n;
-    num2.innerText = m;
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-    setNum();
+    let num1 = document.getElementById("num1");
+    let num2 = document.getElementById("num2");
+    let facileButton = document.getElementById("facile");
+    let intermediaireButton = document.getElementById("intermediaire");
+    let difficileButton = document.getElementById("difficile");
+
+    function facile() {
+        let n = Math.floor(Math.random()*2) + 1;
+        let m = Math.floor(Math.random()*2) + 1;
+        num1.innerText = n;
+        num2.innerText = m;
+    }
+
+    function intermediaire() {
+        let n = Math.floor(Math.random()*5) + 2;
+        let m = Math.floor(Math.random()*5) + 2;
+        num1.innerText = n;
+        num2.innerText = m;
+    }
+
+    function difficile() {
+        let n = Math.floor(Math.random()*10) + 5;
+        let m = Math.floor(Math.random()*10) + 5;
+        num1.innerText = n;
+        num2.innerText = m;
+    }
+
+    facileButton.addEventListener("click", function() {
+        facile();
+    });
+
+    intermediaireButton.addEventListener("click", function() {
+        intermediaire();
+    });
+
+    difficileButton.addEventListener("click", function() {
+        difficile();
+    });
 });
