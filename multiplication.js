fca9b55a1e91ef3eca2ac171b0eda76567008c4a
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-03-28 14:02:05 +0100

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -4,31 +4,37 @@ document.addEventListener("DOMContentLoaded", function() {
     let facileButton = document.getElementById("facile");
     let intermediaireButton = document.getElementById("intermediaire");
     let difficileButton = document.getElementById("difficile");
-    let choixNiveauDiv = document.getElementById("choixNiveau");
+	let choixNiveauDiv = document.getElementById("choixNiveau");
     let appDiv = document.querySelector(".app");
 
     function facile() {
-        let n = Math.floor(Math.random()*2) + 1;
-        let m = Math.floor(Math.random()*2) + 1;
-        num1.innerText = n;
-        num2.innerText = m;
-    }
+		let tables = [1, 2, 10];
+		let tableIndex = Math.floor(Math.random() * tables.length);
+		let n = tables[tableIndex];
+		let m = Math.floor(Math.random() * 10) + 1;
+		num1.innerText = n;
+		num2.innerText = m;
+	}
 
     function intermediaire() {
-        let n = Math.floor(Math.random()*5) + 2;
-        let m = Math.floor(Math.random()*5) + 2;
-        num1.innerText = n;
-        num2.innerText = m;
-    }
+		let tables = [5, 3];
+		let tableIndex = Math.floor(Math.random() * tables.length);
+		let n = tables[tableIndex];
+		let m = Math.floor(Math.random() * 10) + 1;
+		num1.innerText = n;
+		num2.innerText = m;
+	}
 
-    function difficile() {
-        let n = Math.floor(Math.random()*10) + 5;
-        let m = Math.floor(Math.random()*10) + 5;
-        num1.innerText = n;
-        num2.innerText = m;
-    }
+	function difficile() {
+		let tables = [4, 6, 7, 8, 9];
+		let tableIndex = Math.floor(Math.random() * tables.length);
+		let n = tables[tableIndex];
+		let m = Math.floor(Math.random() * 10) + 1;
+		num1.innerText = n;
+		num2.innerText = m;
+	}
 
-    facileButton.addEventListener("click", function() {
+	facileButton.addEventListener("click", function() {
         appDiv.style.display = "block";
         choixNiveauDiv.style.display = "none";
         facile();
