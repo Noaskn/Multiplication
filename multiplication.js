17fc7ef00a4be577d6470f6e1d6e2882f84f268c
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-04-02 14:12:39 +0200

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let intermediaireButton = document.getElementById("intermediaire");
     let difficileButton = document.getElementById("difficile");
     let reselectNiveauButton = document.getElementById("reselectNiveau");
-	let choixNiveauDiv = document.getElementById("choixNiveau");
+    let choixNiveauDiv = document.getElementById("choixNiveau");
     let appDiv = document.querySelector(".app");
     let validerButton = document.getElementById("valider");
     let resultatPara = document.getElementById("resultat");
@@ -14,78 +14,87 @@ document.addEventListener("DOMContentLoaded", function() {
     let niveau = "";
     let finDuJeuButton = document.getElementById("finDuJeu");
     let timerElement = document.getElementById('timer');
-    let debutJeu = Date.now(); // Temps de début du jeu
+    let debutJeu = Date.now();
     let timerInterval;
-	let dernierTempsReponse = debutJeu;
-	let correctResponses = 0;
+    let dernierTempsReponse = debutJeu;
+    let correctResponses = 0;
+    let derniersIncorrects = [];
 
     document.getElementById("input").addEventListener("keypress", function(event) {
         if (event.key === "Enter") {
-            event.preventDefault(); // Empêcher le comportement par défaut de la touche "Entrée"
-            validerButton.click(); // Simuler un clic sur le bouton de validation
+            event.preventDefault();
+            validerButton.click();
         }
     });
 
+    function genererOperation(tables) {
+        let choix = Math.random();
+        if (choix < 0.9) {
+            let n = tables[Math.floor(Math.random() * tables.length)];
+            let m = Math.floor(Math.random() * 10) + 1;
+            return { num1: n, num2: m };
+        }
+        else if (derniersIncorrects.length > 0) {
+            let indexIncorrect = Math.floor(Math.random() * derniersIncorrects.length);
+            return derniersIncorrects[indexIncorrect];
+        }
+        else {
+            let n = tables[Math.floor(Math.random() * tables.length)];
+            let m = Math.floor(Math.random() * 10) + 1;
+            return { num1: n, num2: m };
+        }
+    }
+
     function facile() {
-		let tables = [1, 2, 10];
-		let tableIndex = Math.floor(Math.random() * tables.length);
-		let n = tables[tableIndex];
-		let m = Math.floor(Math.random() * 10) + 1;
-		num1.innerText = n;
-		num2.innerText = m;
-        demarrerChronometre(); // Démarrer le chronomètre
+        let operation = genererOperation([1, 2, 10]);
+        num1.innerText = operation.num1;
+        num2.innerText = operation.num2;
+        demarrerChronometre();
     }
 
     function intermediaire() {
-		let tables = [5, 3];
-		let tableIndex = Math.floor(Math.random() * tables.length);
-		let n = tables[tableIndex];
-		let m = Math.floor(Math.random() * 10) + 1;
-		num1.innerText = n;
-		num2.innerText = m;
-        demarrerChronometre(); // Démarrer le chronomètre
-	}
-
-    function difficile() {
-		let tables = [4, 6, 7, 8, 9];
-		let tableIndex = Math.floor(Math.random() * tables.length);
-		let n = tables[tableIndex];
-		let m = Math.floor(Math.random() * 10) + 1;
-		num1.innerText = n;
-		num2.innerText = m;
-        demarrerChronometre(); // Démarrer le chronomètre
+        let operation = genererOperation([5, 3]);
+        num1.innerText = operation.num1;
+        num2.innerText = operation.num2;
+        demarrerChronometre();
     }
 
-	facileButton.addEventListener("click", function() {
-		appDiv.style.display = "block";
-		choixNiveauDiv.style.display = "none";
-		facile();
-		niveau = "facile";
-		if (!timerInterval) {
-			demarrerChronometre(); // Démarrer le chronomètre uniquement si ce n'est pas déjà fait
-		}
-	});
+    function difficile() {
+        let operation = genererOperation([4, 6, 7, 8, 9]);
+        num1.innerText = operation.num1;
+        num2.innerText = operation.num2;
+        demarrerChronometre();
+    }
 
-	intermediaireButton.addEventListener("click", function() {
-		appDiv.style.display = "block";
-		choixNiveauDiv.style.display = "none";
-		intermediaire();
-		niveau = "intermediaire";
-		if (!timerInterval) {
-			demarrerChronometre(); // Démarrer le chronomètre uniquement si ce n'est pas déjà fait
-		}
-	});
+    facileButton.addEventListener("click", function() {
+        appDiv.style.display = "block";
+        choixNiveauDiv.style.display = "none";
+        facile();
+        niveau = "facile";
+        if (!timerInterval) {
+            demarrerChronometre();
+        }
+    });
 
-	difficileButton.addEventListener("click", function() {
-		appDiv.style.display = "block";
-		choixNiveauDiv.style.display = "none";
-		difficile();
-		niveau = "difficile";
-		if (!timerInterval) {
-			demarrerChronometre(); // Démarrer le chronomètre uniquement si ce n'est pas déjà fait
-		}
-	});
+    intermediaireButton.addEventListener("click", function() {
+        appDiv.style.display = "block";
+        choixNiveauDiv.style.display = "none";
+        intermediaire();
+        niveau = "intermediaire";
+        if (!timerInterval) {
+            demarrerChronometre();
+        }
+    });
 
+    difficileButton.addEventListener("click", function() {
+        appDiv.style.display = "block";
+        choixNiveauDiv.style.display = "none";
+        difficile();
+        niveau = "difficile";
+        if (!timerInterval) {
+            demarrerChronometre();
+        }
+    });
 
     reselectNiveauButton.addEventListener("click", function() {
         appDiv.style.display = "none";
@@ -104,76 +113,75 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function demarrerChronometre() {
-        clearInterval(timerInterval); // Arrêter le chronomètre précédent s'il y en a un
+        clearInterval(timerInterval);
         timerInterval = setInterval(() => {
-			let maintenant = Date.now();
-			let tempsEcoule = Math.floor((maintenant - debutJeu) / 1000); // Temps écoulé depuis le début du jeu en secondes
-			afficherTemps(tempsEcoule);
+            let maintenant = Date.now();
+            let tempsEcoule = Math.floor((maintenant - debutJeu) / 1000);
+            afficherTemps(tempsEcoule);
         }, 1000);
     }
 
+    validerButton.addEventListener("click", function() {
+        event.preventDefault();
+        let reponse = document.getElementById("input").value;
+        let resultat = parseInt(reponse) === parseInt(num1.innerText) * parseInt(num2.innerText);
+        let tempsReponse = Math.floor((Date.now() - dernierTempsReponse) / 1000);
 
+        clearInterval(timerInterval);
 
+        if (resultat) {
+            let scoreToAdd = 0;
+            if (tempsReponse < 3) {
+                scoreToAdd = 3;
+            }
+            else if (tempsReponse >= 3 && tempsReponse < 5) {
+                scoreToAdd = 2;
+            }
+            else {
+                scoreToAdd = 1;
+            }
 
-	validerButton.addEventListener("click", function() {
-		event.preventDefault();
-		let reponse = document.getElementById("input").value;
-		let resultat = parseInt(reponse) === parseInt(num1.innerText) * parseInt(num2.innerText);
+            if (correctResponses >= 9) {
+                scoreToAdd *= 2;
+                document.getElementById("bonusMessage").innerText = "Bonus x2 activé !";
+            }
 
-		// Calcul du temps écoulé depuis la dernière réponse en secondes
-		let tempsReponse = Math.floor((Date.now() - dernierTempsReponse) / 1000);
+            score += scoreToAdd;
+            correctResponses++;
+            resultatPara.innerText = "Bonne réponse !";
+        }
+        
+        else {
+            resultatPara.innerText = "Mauvaise réponse.";
+            correctResponses = 0; 
+            let scoreToRemove = 0;
+            if (tempsReponse < 3) {
+                scoreToRemove = -1;
+            }
+            else if (tempsReponse >= 3 && tempsReponse < 5) {
+                scoreToRemove = -2;
+            }
+            else {
+                scoreToRemove = -3;
+            }
+            score += scoreToRemove;
+            document.getElementById("bonusMessage").innerText = ""; 
+            derniersIncorrects.push({ num1: parseInt(num1.innerText), num2: parseInt(num2.innerText) });
+        }
 
-		clearInterval(timerInterval); // Arrêter le chronomètre
+        scoreDisplay.innerText = score;
+        document.getElementById("input").value = "";
+        demarrerChronometre();
+        dernierTempsReponse = Date.now();
 
-		if (resultat) {
-			// Calcul du score en fonction du temps de réponse
-			let scoreToAdd = 0;
-			if (tempsReponse < 3) {
-				scoreToAdd = 3;
-			} else if (tempsReponse >= 3 && tempsReponse < 5) {
-				scoreToAdd = 2;
-			} else {
-				scoreToAdd = 1;
-			}
-
-        // Appliquer le bonus x2 sur le score à ajouter
-			if (correctResponses >= 9) {
-				scoreToAdd *= 2;
-            document.getElementById("bonusMessage").innerText = "Bonus x2 activé !";
-			}
-
-        score += scoreToAdd; // Ajouter le score calculé
-        correctResponses++; // Incrémentez le nombre de réponses correctes consécutives
-        resultatPara.innerText = "Bonne réponse !";
-		} else {
-			resultatPara.innerText = "Mauvaise réponse.";
-			correctResponses = 0; 
-			let scoreToRemove = 0;
-			if (tempsReponse < 3) {
-				scoreToRemove = -1;
-			} else if (tempsReponse >= 3 && tempsReponse < 5) {
-				scoreToRemove = -2;
-			} else {
-				scoreToRemove = -3;
-			}
-			score += scoreToRemove;
-			document.getElementById("bonusMessage").innerText = ""; 
-		}
-
-		scoreDisplay.innerText = score;
-		document.getElementById("input").value = "";
-		demarrerChronometre(); // Redémarrer le chronomètre
-
-		// Enregistrer le moment de la dernière réponse
-		dernierTempsReponse = Date.now();
-		if (niveau === "facile") {
-			facile();
-		} else if (niveau === "intermediaire") {
-			intermediaire();
-		} else if (niveau === "difficile") {
-			difficile();
-		}
-	});
-
-
+        if (niveau === "facile") {
+            facile();
+        }
+        else if (niveau === "intermediaire") {
+            intermediaire();
+        }
+        else if (niveau === "difficile") {
+            difficile();
+        }
+    });
 });
