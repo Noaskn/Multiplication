fdbd1909f85bf705055c5983b3393b85f1aa8aea
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-05-06 10:28:30 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,36 @@
+<?php
+include("parametre.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    if (!empty($username) && !empty($password)) {
+        $file = 'utilisateurs.txt';
+        $fp = fopen($file, 'r');
+        while (!feof($fp)) {
+            $line = fgets($fp);
+            $parts = explode(';', $line);
+            if ($parts[0] == $username && $parts[1] == $password) {
+                fclose($fp);
+                include("multiplication.php");
+                exit();
+            }
+        }
+        echo "Identifiants invalides. Veuillez réessayer.";
+        fclose($fp);
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles.css">
-    <title>Connexion</title>
+    <title>Inscription</title>
 </head>
 <body>
     <h2>Connexion</h2>
-    <form action="/connexion" method="post">
+    <form action=<?php echo $url."/login.php"; ?> method="post">
         <div>
             <label for="username">Nom d'utilisateur :</label>
             <input type="text" id="username" name="username" required>
@@ -17,7 +40,7 @@
             <input type="password" id="password" name="password" required>
         </div>
         <div>
-            <input type="submit" value="Se connecter">
+            <input type="submit" value="Cr&eacute;er un compte">
         </div>
     </form>
 </body>
