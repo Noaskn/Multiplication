d8472c2f3c9e847300e110051651830dbab66c72
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-05-12 13:12:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,33 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
     <title>Page principale</title>
 </head>
 <body>
-    <p><h2>Voulez-vous vous connecter ou cr&eacute;er un compte ?</h2></p>
-    <a href="signup.html" class="button">Créer un compte</a>
-    <a href="login.html" class="button">Se connecter</a>
+    <p><h2>Voulez-vous vous connecter ou créer un compte ?</h2></p>
+    <a href="signup.php" class="button">Créer un compte</a>
+    <a href="login.php" class="button">Se connecter</a>
+
+    <h2>Espace administrateur</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="code">Entrez votre code :</label>
+        <input type="number" id="code" name="code" required>
+        <button type="submit">Valider</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $codeSaisi = $_POST["code"] ?? '';
+        $fichierCodesAdmin = "administrateur.txt";
+        $contenuFichier = file_get_contents($fichierCodesAdmin);
+        if (strpos($contenuFichier, $codeSaisi) !== false) {
+            header("Location: admin.php");
+            exit();
+        }
+        else {
+            echo "<p>Code incorrect. Veuillez réessayer.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
