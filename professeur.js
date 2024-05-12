305b91730b0cfbd177a5da42c59b5ca08ae79760
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-12 16:35:21 +0200

    Update professeur.js

--- professeur.js
+++ professeur.js
@@ -8,22 +8,29 @@ function genererCode() {
         }
 
         function enregistrer() {
-            var code = document.getElementById("code").value;
-            var niveau = document.querySelector('input[name="niveau"]:checked').value;
+    var code = document.getElementById("code").value;
+    var niveau = document.querySelector('input[name="niveau"]:checked').value;
 
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        var messageDiv = document.getElementById("message");
-                        messageDiv.innerText = "Le code a été enregistré avec succès.";
-                        messageDiv.style.display = "block";
-                    } else {
-                        alert('Une erreur s\'est produite lors de l\'enregistrement.');
-                    }
-                }
-            };
-            xhr.open("POST", "professeur.php", true);
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send("code=" + code + "&niveau=" + niveau);
+    // Vérifier si le champ du code est vide
+    if (code.trim() === "") {
+        alert("Veuillez générer un code avant de continuer.");
+        return; // Stoppe l'exécution de la fonction
+    }
+
+    // Envoyer les données uniquement si le champ du code n'est pas vide
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var messageDiv = document.getElementById("message");
+                messageDiv.innerText = "Le code a été enregistré avec succès.";
+                messageDiv.style.display = "block";
+            } else {
+                alert('Une erreur s\'est produite lors de l\'enregistrement.');
+            }
         }
+    };
+    xhr.open("POST", "professeur.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("code=" + code + "&niveau=" + niveau);
+}
