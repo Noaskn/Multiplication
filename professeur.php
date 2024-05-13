b0363e32414ad7aa8a1e886d14361b367e5830fb
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-13 10:40:45 +0200

    Update professeur.php

--- professeur.php
+++ professeur.php
@@ -26,85 +26,102 @@
 	
 	<p>Indiquez le code de votre classe :</p>
     
-    <form action=<?php echo $url."/professeur.php"; ?> method="post">
+    <form action="<?php echo $url."/professeur.php"; ?>" method="post">
         <input type="text" name="code_classe" placeholder="Entrez le code de votre classe" required>
         <button type="submit_classe" name="afficher_resultats">Afficher les résultats</button>
     </form>
 
     <div id="donnees_classe">
-      <?php
-include("parametre.php");
+        <?php
+        include("parametre.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["afficher_resultats"])) {
-    $code_classe = $_POST["code_classe"] ?? '';
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["afficher_resultats"])) {
+            $code_classe = $_POST["code_classe"] ?? '';
 
-    $fichier_nom = "$code_classe.txt";
-    if (file_exists($fichier_nom)) {
-        $contenu = file($fichier_nom);
+            $fichier_nom = "$code_classe.txt";
+            if (file_exists($fichier_nom)) {
+                $contenu = file($fichier_nom);
 
-        $donnees_scores = [];
-        foreach ($contenu as $ligne) {
-            $donnees = explode(";", $ligne);
-            $score = intval($donnees[3]); 
-            $donnees_scores[$score] = $donnees;
+                $donnees_scores = [];
+                foreach ($contenu as $ligne) {
+                    $donnees = explode(";", $ligne);
+                    $score = intval($donnees[3]); 
+                    $donnees_scores[$score] = $donnees;
+                }
+
+                krsort($donnees_scores);
+
+                echo "<table border='1'>";
+                echo "<caption>HIGHSCORE</caption>";
+                echo "<tr>";
+                echo "<th>Classement</th>";
+                echo "<th>Nom de l'utilisateur</th>";
+                echo "<th>Score</th>";
+                echo "</tr>";
+
+                $classement = 1;
+                foreach ($donnees_scores as $donnees) {
+                    echo "<tr>";
+                    echo "<td>".$classement."</td>"; 
+                    echo "<td>".$donnees[0]."</td>"; 
+                    echo "<td>".$donnees[3]."</td>"; 
+                    echo "</tr>";
+                    $classement++; 
+                }
+                echo "</table>";
+            } else {
+                echo "Aucune donnée trouvée pour ce code de classe.";
+            }
         }
-
-        krsort($donnees_scores);
-
-        echo "<table border='1'>";
-        echo "<caption>HIGHSCORE</caption>";
-        echo "<tr>";
-        echo "<th>Classement</th>";
-        echo "<th>Nom de l'utilisateur</th>";
-        echo "<th>Score</th>";
-        echo "</tr>";
-
-        $classement = 1;
-        foreach ($donnees_scores as $donnees) {
-            echo "<tr>";
-            echo "<td>".$classement."</td>"; 
-            echo "<td>".$donnees[0]."</td>"; 
-            echo "<td>".$donnees[3]."</td>"; 
-            echo "</tr>";
-            $classement++; 
-        }
-        echo "</table>";
-    } else {
-        echo "Aucune donnée trouvée pour ce code de classe.";
-    }
-}
-?>
-
-
+        ?>
     </div>
+
+    <?php if (!empty($_POST['code_classe'])): ?>
+    <div id="recherche_utilisateur">
+        <h2>Recherche d'utilisateur</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type="hidden" name="code_classe" value="<?php echo isset($_POST['code_classe']) ? $_POST['code_classe'] : ''; ?>">
+            <label for="search_user">Rechercher un utilisateur :</label>
+            <input type="text" id="search_user" name="search_user">
+            <input type="submit" value="Rechercher">
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search_user"])) {
+            $search_term = $_POST["search_user"];
+            $code_classe = $_POST["code_classe"];
+
+            if (!empty($search_term) && !empty($code_classe)) {
+                $fichier_nom = "$code_classe.txt";
+                if (file_exists($fichier_nom)) {
+                    $contenu = file($fichier_nom);
+                    $found = false;
+
+                    foreach ($contenu as $ligne) {
+                        $donnees = explode(";", $ligne);
+                        if (stripos($donnees[0], $search_term) !== false) {
+                            echo "<p>Résultat de la recherche : <p>";
+							echo "<p>Nom d'utilisateur : ".$donnees[0]."<p>";
+							echo "<p>Score : ".$donnees[3]."</p>";
+                            $found = true;
+                        }
+                    }
+
+                    if (!$found) {
+                        echo "Utilisateur non trouvé.";
+                    }
+                } else {
+                    echo "Aucune donnée trouvée pour ce code de classe.";
+                }
+            } else {
+                echo "Veuillez entrer un terme de recherche et un code de classe valide.";
+            }
+        }
+        ?>
+    </div>
+    <?php endif; ?>
 
     <div id="message" style="display:none;"></div>
 	<script type="text/javascript" src="professeur.js"></script>
 
 </body>
 </html>
-
-
-<?php
-include("parametre.php");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $code = $_POST["code"] ?? '';
-    $niveau = $_POST["niveau"] ?? '';
-
-    $contenu = "$code $niveau\n";
-
-    $fichier = fopen("groupe.txt", "a+") or die("Impossible d'ouvrir le fichier.");
-
-    // Vérifier si les données existent déjà dans le fichier
-    $contenu_fichier = file_get_contents("groupe.txt");
-    if (strpos($contenu_fichier, $contenu) === false) {
-        fwrite($fichier, $contenu);
-    }
-
-    fclose($fichier);
-
-    exit;
-}
-?>
-
