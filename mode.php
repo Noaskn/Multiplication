0ab1c96916a51f57a3c1bf149f18a8be88a3feb2
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-05-15 10:11:33 +0200

    Create mode.php

---
+++ mode.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['mode'] == 'solo') {
+        header('Location: multiplication.php');
+        exit();
+    }
+    elseif ($_POST['mode'] == 'groupe') {
+        header('Location: groupe.php');
+        exit();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mode de Jeu</title>
+</head>
+<body>
+    <form action="mode.php" method="post">
+        <p>Sélectionnez votre mode de jeu :</p>
+        <input type="radio" id="solo" name="mode" value="solo">
+        <label for="solo">Solo</label><br>
+        <input type="radio" id="groupe" name="mode" value="groupe">
+        <label for="groupe">Groupe</label><br>
+        <input type="submit" value="Valider">
+    </form>
+</body>
+</html>
