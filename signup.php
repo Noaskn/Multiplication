95d974a11818c7131db3b26ff78bc06fc5255fe3
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-05-16 18:52:00 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,30 +1,26 @@
 <?php
-session_start();
-include("parametre.php");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
-    $type = $_POST['type'] ?? '';
-    $bloque = 'Non';
-
-    if (!empty($username) && !empty($password)) {
-        $random_id = uniqid();
-        $_SESSION['random_id'] = $random_id;
-        $data = "$username;$password;$random_id;0;$type;$bloque\n";
-        $file = 'utilisateurs.txt';
-
-        file_put_contents($file, $data, FILE_APPEND);
-
-        if ($type == "Professeur"){
-            include("professeur.php");
+    session_start();
+    include("parametre.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
+        $type = $_POST['type'] ?? '';
+        $bloque = 'Non';
+        if(!empty($username) && !empty($password)){
+            $random_id = uniqid();
+            $_SESSION['random_id'] = $random_id;
+            $data = "$username;$password;$random_id;0;$type;$bloque\n";
+            $file = 'utilisateurs.txt';
+            file_put_contents($file, $data, FILE_APPEND);
+            if($type == "Professeur"){
+                include("professeur.php");
+            }
+            else{
+                include("mode.php");
+            }
+            exit();
         }
-        else {
-            include("mode.php");
-        }
-        exit();
     }
-}
 ?>
 
 <!DOCTYPE html>
