a75f943078413e80bf30881acab6273157c9480f
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-16 20:54:03 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,37 +1,59 @@
 <?php
     session_start();
     include("parametre.php");
+    $message = '';
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username = $_POST['username'] ?? '';
         $password = $_POST['password'] ?? '';
         $type = $_POST['type'] ?? '';
         $bloque = 'Non';
         if(!empty($username) && !empty($password)){
-            $random_id = uniqid();
-            $_SESSION['random_id'] = $random_id;
-            $data = "$username;$password;$random_id;0;$type;$bloque\n";
-            $file = 'utilisateurs.txt';
-            file_put_contents($file, $data, FILE_APPEND);
-            if($type == "Professeur"){
-                include("professeur.php");
+            $utilisateurs = array_filter(file("utilisateurs.txt", FILE_IGNORE_NEW_LINES));
+            $username_exists = false;
+            foreach($utilisateurs as $utilisateur){
+                $info = explode(";", $utilisateur);
+                if ($info[0] == $username) {
+                    $username_exists = true;
+                    break;
+                }
+            }
+            if($username_exists){
+                $message = "Le nom d'utilisateur existe déjà. Veuillez en choisir un autre.";
             }
             else{
-                include("mode.php");
+                $random_id = uniqid();
+                $_SESSION['random_id'] = $random_id;
+                $data = "$username;$password;$random_id;0;$type;$bloque\n";
+                $file = 'utilisateurs.txt';
+                file_put_contents($file, $data, FILE_APPEND);
+                if($type == "Professeur"){
+                    include("professeur.php");
+                }
+                else{
+                    include("mode.php");
+                }
+                exit();
             }
-            exit();
         }
     }
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Inscription</title>
-    <link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h2>Créer un compte</h2>
+	
+	 <?php if (!empty($message)): ?>
+        <p><?php echo $message; ?></p>
+    <?php endif; ?>
+	
     <form action="<?php echo $url."/signup.php"; ?>" class="basique" method="post">
         <div class="debut">
             <label for="username">Nom d'utilisateur :</label>
