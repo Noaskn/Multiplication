df9765546e74c451417e940a7df6b4a069af893f
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-17 11:33:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Page principale</title>
-    <link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <p><h2>Voulez-vous vous connecter ou créer un compte ?</h2></p>
     <a href="signup.php" class="button">Créer un compte</a>
     <a href="login.php" class="button">Se connecter</a>
+
     <h2>Espace administrateur</h2>
     <form class="codeadm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="code">Entrez votre code :</label>
@@ -17,19 +18,18 @@
     </form>
 
     <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $codeSaisi = $_POST["code"] ?? '';
-            $fichierCodesAdmin = "administrateur.txt";
-            $contenuFichier = file_get_contents($fichierCodesAdmin);
-            if(strpos($contenuFichier, $codeSaisi) !== false){
-                header("Location: admin.php");
-                exit();
-            }
-            else{
-                echo "<p>Code incorrect. Veuillez réessayer.</p>";
-            }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $codeSaisi = $_POST["code"] ?? '';
+        $fichierCodesAdmin = "administrateur.txt";
+        $contenuFichier = file_get_contents($fichierCodesAdmin);
+        if (strpos($contenuFichier, $codeSaisi) !== false) {
+            header("Location: admin.php");
+            exit();
         }
+        else {
+            echo "<p>Code incorrect. Veuillez réessayer.</p>";
+        }
+    }
     ?>
-
 </body>
 </html>
