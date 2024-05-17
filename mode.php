a2fc7334d1136fa7ad15bcc485870eb9846ce73e
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-17 11:35:49 +0200

    Update mode.php

--- mode.php
+++ mode.php
@@ -1,16 +1,14 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if($_POST['mode'] == 'solo'){
-            header('Location: multiplication.php');
-            exit();
-        }
-        elseif($_POST['mode'] == 'groupe'){
-            header('Location: groupe.php');
-            exit();
-        }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['mode'] == 'solo') {
+        header('Location: multiplication.php');
+        exit();
+    } elseif ($_POST['mode'] == 'groupe') {
+        header('Location: groupe.php');
+        exit();
     }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -20,11 +18,12 @@
 </head>
 <body>
     <form class='niveau' action="mode.php" method="post">
-        <p>Sélectionnez votre mode de jeu :</p>
+        <h4>Sélectionnez votre mode de jeu :</h4>
         <input type="radio" id="solo" name="mode" value="solo">
         <label for="solo">Solo</label><br>
         <input type="radio" id="groupe" name="mode" value="groupe">
         <label for="groupe">Groupe</label><br>
+		<br>
         <input type="submit" value="Valider">
     </form>
     <a href="password.php">Changer le mot de passe</a>
