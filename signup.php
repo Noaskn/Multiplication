b87363ffb0932ca2b73c075bb7f74728a4296efd
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-17 10:23:23 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,44 +1,48 @@
 <?php
-    session_start();
-    include("parametre.php");
-    $message = '';
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $username = $_POST['username'] ?? '';
-        $password = $_POST['password'] ?? '';
-        $type = $_POST['type'] ?? '';
-        $bloque = 'Non';
-        if(!empty($username) && !empty($password)){
-            $utilisateurs = array_filter(file("utilisateurs.txt", FILE_IGNORE_NEW_LINES));
-            $username_exists = false;
-            foreach($utilisateurs as $utilisateur){
-                $info = explode(";", $utilisateur);
-                if ($info[0] == $username) {
-                    $username_exists = true;
-                    break;
-                }
-            }
-            if($username_exists){
-                $message = "Le nom d'utilisateur existe déjà. Veuillez en choisir un autre.";
-            }
-            else{
-                $random_id = uniqid();
-                $_SESSION['random_id'] = $random_id;
-                $data = "$username;$password;$random_id;0;$type;$bloque\n";
-                $file = 'utilisateurs.txt';
-                file_put_contents($file, $data, FILE_APPEND);
-                if($type == "Professeur"){
-                    include("professeur.php");
-                }
-                else{
-                    include("mode.php");
-                }
-                exit();
+session_start();
+include("parametre.php");
+$message = '';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $type = $_POST['type'] ?? '';
+    $bloque = 'Non';
+    
+    if (!empty($username) && !empty($password)) {
+        $file_path = "utilisateurs.txt";
+        if (file_exists($file_path)) {
+            $utilisateurs = array_filter(file($file_path, FILE_IGNORE_NEW_LINES));
+        } else {
+            $utilisateurs = []; 
+        }
+
+        $username_exists = false;
+        foreach ($utilisateurs as $utilisateur) {
+            $info = explode(";", $utilisateur);
+            if ($info[0] == $username) {
+                $username_exists = true;
+                break;
             }
         }
+
+        if ($username_exists) {
+            $message = "Le nom d'utilisateur existe déjà. Veuillez en choisir un autre.";
+        } else {
+            $random_id = uniqid();
+            $_SESSION['random_id'] = $random_id;
+            $data = "$username;$password;$random_id;0;$type;$bloque\n";
+            file_put_contents($file_path, $data, FILE_APPEND);
+
+            if ($type == "Professeur") {
+                include("professeur.php");
+            } else {
+                include("mode.php");
+            }
+            exit();
+        }
     }
+}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="fr">
