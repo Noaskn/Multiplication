4b3a63111ba8733681f39c4bddad9f45225ffd90
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-20 17:00:29 +0200

    Update multiplication.php

--- multiplication.php
+++ multiplication.php
@@ -6,34 +6,39 @@ $url='';
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Solo</title>
-		<link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <div id="choixNiveau">
-            <p><h2>Choisissez le niveau de difficulté</h2></p>
-            <a href="#" class="button" id="facile">Facile</a>
-            <a href="#" class="button" id="intermediaire">Intermédiaire</a>
-            <a href="#" class="button" id="difficile">Difficile</a>
-        </div>
-        <div class="app" style="display:none;">
-            <header>
-                <span class="score">Score : <span id="score"> 0</span></span>
-            </header>
-            <h2><span id="num1">0</span> x <span id="num2">0</span> ? </h2>
-            <input class='reponse' type="number" placeholder="Entrer la réponse" id="input">
-            <button id="valider" class="valider">Valider</button>
-            <div id="timer">00:00</div>
-			<p id="bonusMessage"></p>
+<head>
+    <meta charset="UTF-8">
+    <title>Mode Solo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="choixNiveau">
+        <p><h2>Choisissez le niveau de difficulté</h2></p>
+        <a href="#" class="button" id="facile">Facile</a>
+        <a href="#" class="button" id="intermediaire">Intermédiaire</a>
+        <a href="#" class="button" id="difficile">Difficile</a>
+    </div>
+    <div class="app" style="display:none;">
+        <header>
+            <span class="score">Score : <span id="score"> 0</span></span>
+        </header>
+        <h2><span id="num1">0</span> x <span id="num2">0</span> ? </h2>
+        <input class='reponse' type="number" placeholder="Entrer la réponse" id="input">
+        <button id="valider" class="valider">Valider</button>
+        <div id="timer">00:00</div>
+        <br>
+        <p class='niveau' id="bonusMessage"></p>
+        <p class='niveau' id="resultat"></p>
+        <br>
+        <div style="display: flex; justify-content: space-between;">
             <button class="bouton" id="reselectNiveau">Rechoisir le niveau</button>
-            <script>
-                var uniqid = "<?php echo isset($_SESSION['random_id']) ? $_SESSION['random_id'] : ''; ?>";
-            </script>
             <a class="bouton" id='finDuJeu' href="<?php echo $url."/score.php"; ?>">Fin du jeu</a>
-            <p id="resultat"></p>
         </div>
-        <script type="text/javascript" src="multiplication.js"></script>
-    </body>
+        <br>
+        <script>
+            var uniqid = "<?php echo isset($_SESSION['random_id']) ? $_SESSION['random_id'] : ''; ?>";
+        </script>
+    </div>
+    <script type="text/javascript" src="multiplication.js"></script>
+</body>
 </html>
