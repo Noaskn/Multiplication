a2865bc325a519cffc8ba3a0e52af9c76ff3ba90
Noaskn <133594763+Noaskn@users.noreply.github.com>
2024-05-20 18:11:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
 </head>
 <body>
     <p><h2>Bienvenue sur le jeu de multiplication</h2></p>
+
+    <!-- Liens vers les pages de création de compte et de connexion -->
     <a href="signup.php" class="button">Créer un compte</a>
     <a href="login.php" class="button">Se connecter</a>
-
     <h2>Espace administrateur</h2>
+
+    <!-- Formulaire pour entrer le code administrateur -->
     <form class="codeadm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="code">Entrez votre code :</label>
 		<br>
@@ -18,17 +21,24 @@
 		<br>
         <button class="submitcompte" type="submit">Valider</button>
     </form>
-
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        // Récupérer le code saisi par l'utilisateur
         $codeSaisi = $_POST["code"] ?? '';
         $fichierCodesAdmin = "administrateur.txt";
         $contenuFichier = file_get_contents($fichierCodesAdmin);
-        if (strpos($contenuFichier, $codeSaisi) !== false) {
+
+        // Vérifier si le code saisi est présent dans le fichier
+        if(strpos($contenuFichier, $codeSaisi) !== false){
+
+            // Rediriger vers la page admin si le code est correct
             header("Location: admin.php");
             exit();
         }
-        else {
+        else{
+
+            // Afficher un message d'erreur si le code est incorrect
             echo "<p>Code incorrect. Veuillez réessayer.</p>";
         }
     }
