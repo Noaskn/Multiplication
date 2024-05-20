a388b2842e3b5010666ab5dfdaef06f28cb4f044
alicefranco7 <158499815+alicefranco7@users.noreply.github.com>
2024-05-20 16:59:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <p><h2>Voulez-vous vous connecter ou créer un compte ?</h2></p>
+    <p><h2>Bienvenue sur le jeu de multiplication</h2></p>
     <a href="signup.php" class="button">Créer un compte</a>
     <a href="login.php" class="button">Se connecter</a>
 
     <h2>Espace administrateur</h2>
     <form class="codeadm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="code">Entrez votre code :</label>
+		<br>
         <input type="number" id="code" name="code" required>
+		<br>
         <button class="submitcompte" type="submit">Valider</button>
     </form>
 
